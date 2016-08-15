ea6d8d38236b6cbe885671c20679d430503bf170
Elie Zananiri <prisonerjohn@gmail.com>
2016-08-15 16:27:25 +0200

    Spaces to tabs.

--- src/ofxRenderToolkit/pbr/Material.cpp
+++ src/ofxRenderToolkit/pbr/Material.cpp
@@ -2,25 +2,25 @@
 
 namespace ofxRenderToolkit
 {
-    namespace pbr
-    {
-        //--------------------------------------------------------------
-        Material::Material()
-            : updateFlags(0)
-            , name("")
-            , baseColor("Base Color", ofFloatColor::black)
-            , metallic("Metallic", 0.0f, 0.0f, 1.0f)
-            , roughness("Roughness", 0.0f, 0.0f, 1.0f)
-            , emissiveColor("Emissive Color", ofFloatColor::black)
-            , emissiveIntensity("Emissive Intensity", 0.0f, 0.0f, 1.0f)
-            , texAlbedo(nullptr)
-            , texNormal(nullptr)
-            , texMetalness(nullptr)
-            , texRoughness(nullptr)
-        {
+	namespace pbr
+	{
+		//--------------------------------------------------------------
+		Material::Material()
+			: updateFlags(0)
+			, name("")
+			, baseColor("Base Color", ofFloatColor::black)
+			, metallic("Metallic", 0.0f, 0.0f, 1.0f)
+			, roughness("Roughness", 0.0f, 0.0f, 1.0f)
+			, emissiveColor("Emissive Color", ofFloatColor::black)
+			, emissiveIntensity("Emissive Intensity", 0.0f, 0.0f, 1.0f)
+			, texAlbedo(nullptr)
+			, texNormal(nullptr)
+			, texMetalness(nullptr)
+			, texRoughness(nullptr)
+		{
 			this->parameters.setName("Material");
 			this->parameters.add(baseColor, metallic, roughness, emissiveColor, emissiveIntensity);
-        
+
 			this->eventListeners.push_back(this->baseColor.newListener([this](ofFloatColor &)
 			{
 				this->updateFlags |= Material::UPDATE_BASE_COLOR;
@@ -43,54 +43,54 @@ namespace ofxRenderToolkit
 			}));
 		}
 
-        //--------------------------------------------------------------
-        void Material::clearUpdateFlags()
-        {
-            this->updateFlags = 0;
-        }
+		//--------------------------------------------------------------
+		void Material::clearUpdateFlags()
+		{
+			this->updateFlags = 0;
+		}
 
-        //--------------------------------------------------------------
-        void Material::setName(const std::string & name)
-        {
-            this->name = name;
-        }
+		//--------------------------------------------------------------
+		void Material::setName(const std::string & name)
+		{
+			this->name = name;
+		}
 
-        //--------------------------------------------------------------
-        void Material::setAlbedoMap(shared_ptr<ofTexture> tex)
-        {
-            this->texAlbedo = tex;
-            this->updateFlags |= Material::UPDATE_ALBEDO_MAP;
-        }
+		//--------------------------------------------------------------
+		void Material::setAlbedoMap(shared_ptr<ofTexture> tex)
+		{
+			this->texAlbedo = tex;
+			this->updateFlags |= Material::UPDATE_ALBEDO_MAP;
+		}
 
-        //--------------------------------------------------------------
-        void Material::setNormalMap(shared_ptr<ofTexture> tex)
-        {
-            this->texNormal = tex;
-            this->updateFlags |= Material::UPDATE_NORMAL_MAP;
-        }
+		//--------------------------------------------------------------
+		void Material::setNormalMap(shared_ptr<ofTexture> tex)
+		{
+			this->texNormal = tex;
+			this->updateFlags |= Material::UPDATE_NORMAL_MAP;
+		}
 
-        //--------------------------------------------------------------
-        void Material::setMetalnessMap(shared_ptr<ofTexture> tex)
-        {
-            this->texMetalness = tex;
-            this->updateFlags |= Material::UPDATE_METALNESS_MAP;
-        }
+		//--------------------------------------------------------------
+		void Material::setMetalnessMap(shared_ptr<ofTexture> tex)
+		{
+			this->texMetalness = tex;
+			this->updateFlags |= Material::UPDATE_METALNESS_MAP;
+		}
 
-        //--------------------------------------------------------------
-        void Material::setRoughnessMap(shared_ptr<ofTexture> tex)
-        {
-            this->texRoughness = tex;
-            this->updateFlags |= Material::UPDATE_ROUGHNESS_MAP;
-        }
+		//--------------------------------------------------------------
+		void Material::setRoughnessMap(shared_ptr<ofTexture> tex)
+		{
+			this->texRoughness = tex;
+			this->updateFlags |= Material::UPDATE_ROUGHNESS_MAP;
+		}
 
-        //--------------------------------------------------------------
-        void Material::setUniforms(const ofShader & shader)
-        {
-            shader.setUniform4f("uBaseColor", this->baseColor);
-            shader.setUniform1f("uMetallic", this->metallic);
-            shader.setUniform1f("uRoughness", this->roughness);
-            shader.setUniform4f("uEmissiveColor", this->emissiveColor);
-            shader.setUniform1f("uEmissiveIntensity", this->emissiveIntensity);
-        }
-    }
+		//--------------------------------------------------------------
+		void Material::setUniforms(const ofShader & shader)
+		{
+			shader.setUniform4f("uBaseColor", this->baseColor);
+			shader.setUniform1f("uMetallic", this->metallic);
+			shader.setUniform1f("uRoughness", this->roughness);
+			shader.setUniform4f("uEmissiveColor", this->emissiveColor);
+			shader.setUniform1f("uEmissiveIntensity", this->emissiveIntensity);
+		}
+	}
 }
--- src/ofxRenderToolkit/pbr/Material.h
+++ src/ofxRenderToolkit/pbr/Material.h
@@ -5,37 +5,37 @@
 
 namespace ofxRenderToolkit
 {
-    namespace pbr
-    {
-        class Material
-        {
-        public:
-            Material();
+	namespace pbr
+	{
+		class Material
+		{
+		public:
+			Material();
 
-            void clearUpdateFlags();
+			void clearUpdateFlags();
 
-            void setName(const std::string & name);
+			void setName(const std::string & name);
 
-            void setAlbedoMap(shared_ptr<ofTexture> tex);
-            void setNormalMap(shared_ptr<ofTexture> tex);
-            void setMetalnessMap(shared_ptr<ofTexture> tex);
-            void setRoughnessMap(shared_ptr<ofTexture> tex);
+			void setAlbedoMap(shared_ptr<ofTexture> tex);
+			void setNormalMap(shared_ptr<ofTexture> tex);
+			void setMetalnessMap(shared_ptr<ofTexture> tex);
+			void setRoughnessMap(shared_ptr<ofTexture> tex);
 
-            void setUniforms(const ofShader & shader);
+			void setUniforms(const ofShader & shader);
 
-        public:
-            static const uint8_t UPDATE_BASE_COLOR = 1;
-            static const uint8_t UPDATE_METALLIC = 1 << 1;
-            static const uint8_t UPDATE_ROUGHNESS = 1 << 2;
-            static const uint8_t UPDATE_ALBEDO_MAP = 1 << 3;
-            static const uint8_t UPDATE_NORMAL_MAP = 1 << 4;
-            static const uint8_t UPDATE_METALNESS_MAP = 1 << 5;
-            static const uint8_t UPDATE_ROUGHNESS_MAP = 1 << 6;
+		public:
+			static const uint8_t UPDATE_BASE_COLOR = 1;
+			static const uint8_t UPDATE_METALLIC = 1 << 1;
+			static const uint8_t UPDATE_ROUGHNESS = 1 << 2;
+			static const uint8_t UPDATE_ALBEDO_MAP = 1 << 3;
+			static const uint8_t UPDATE_NORMAL_MAP = 1 << 4;
+			static const uint8_t UPDATE_METALNESS_MAP = 1 << 5;
+			static const uint8_t UPDATE_ROUGHNESS_MAP = 1 << 6;
 
-        public:
-            uint32_t updateFlags;
+		public:
+			uint32_t updateFlags;
 
-            std::string name;
+			std::string name;
 
 			ofParameter<ofFloatColor> baseColor;
 			ofParameter<float> metallic;
@@ -45,14 +45,14 @@ namespace ofxRenderToolkit
 
 			ofParameterGroup parameters;
 
-            // TODO: Texture not implemented yet.
-            ofPtr<ofTexture> texAlbedo;
-            ofPtr<ofTexture> texNormal;
-            ofPtr<ofTexture> texMetalness;
-            ofPtr<ofTexture> texRoughness;
+			// TODO: Texture not implemented yet.
+			ofPtr<ofTexture> texAlbedo;
+			ofPtr<ofTexture> texNormal;
+			ofPtr<ofTexture> texMetalness;
+			ofPtr<ofTexture> texRoughness;
 
 		protected:
 			vector<ofEventListener> eventListeners;
-        };
-    }
+		};
+	}
 }
